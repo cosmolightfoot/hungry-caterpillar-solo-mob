adc9e8a668656325825ef9270c4317f0833d9f4a
Cosmo Kent <cosmolightfoot@gmail.com>
2019-02-01 08:24:46 -0800

    starting work

---
+++ README.md
@@ -0,0 +1,17 @@
+Abstract: 
+Initialize
+Build basic HTML structure
+link elements via id to constants in JS
+
+Build array of colors
+Build color buttons via for loop 
+Build function to add span elements to canvas section
+style classes of colors in css
+
+Build dance buttons via for loop
+Build function that selects all existing span elements of a specific color
+clean up 
+
+branch 
+
+add auto dance functionality
---
+++ src/main.js
@@ -0,0 +1,35 @@
+const colorButtonsNode = document.getElementById('color-buttons');
+const canvasNode = document.getElementById('canvas');
+const danceButtonsNode = document.getElementById('dance-buttons');
+
+const buttons = [
+    {
+        color: 'firebrick',
+        img-src: = ''
+    },
+    {
+        color: 'darkgoldenrod',
+        img-src: = ''
+    },
+    {
+        color: 'darkmagenta',
+        img-src: = ''
+    },
+    {
+        color: 'slateblue',
+        img-src: = ''
+    },
+    {
+        color: 'teal',
+        img-src: = ''
+    },
+]
+//buttons[index].color
+
+for(index = 0; index < buttons.length; index ++) {
+    const colorButton = document.createElement('button');
+    let color = buttons[index].color;
+    console.log(color);
+
+
+}
